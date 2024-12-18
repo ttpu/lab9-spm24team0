d8a405e4bf40d7c2eef143f9e7ad2a95f32d4ac4
Maksudjon Usmonov <maqsudbek94@gmail.com>
2024-12-18 10:41:46 +0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
-print("hello world")
-print("website")
-print("website2")
-print("works")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
